56a394d0dcb05b8720a4bfb2751adc800f7bc419
Santos Leijon <santos.bostrom.leijon@gmail.com>
2023-06-06 20:47:00 +0200

    Add Spring initializr template with Spring Boot and Java 17

---
+++ src/main/java/com/github/santosleijon/voidiummarket/VoidiumMarketApplication.java
@@ -0,0 +1,13 @@
+package com.github.santosleijon.voidiummarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidiumMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidiumMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/santosleijon/voidiummarket/VoidiumMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.santosleijon.voidiummarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoidiumMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
