679211b4780d09f3a1fd5f7643c662587fa66159
Santos Leijon <santos.bostrom.leijon@gmail.com>
2023-08-22 17:39:19 +0200

    Add index on aggregate name

--- db/01_create_event_store_table.sql
+++ db/01_create_event_store_table.sql
@@ -9,3 +9,4 @@ CREATE TABLE IF NOT EXISTS event_store (
 );
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE event_store TO voidium_market;
 CREATE INDEX event_store_aggregate_id_idx ON event_store(aggregate_id);
+CREATE INDEX event_store_aggregate_name_idx ON event_store(aggregate_name);
