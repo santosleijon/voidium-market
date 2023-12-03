3be524eea349e24460de6740ca5e94266356ec73
Santos Leijon <santos.bostrom.leijon@gmail.com>
2023-12-03 13:23:27 +0100

    Add JsonProperty annotations to enable deserialization of events from Kafka

--- src/main/java/com/github/santosleijon/voidiummarket/purchaseorders/events/PurchaseOrderDeleted.java
+++ src/main/java/com/github/santosleijon/voidiummarket/purchaseorders/events/PurchaseOrderDeleted.java
@@ -1,6 +1,7 @@
 package com.github.santosleijon.voidiummarket.purchaseorders.events;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.github.santosleijon.voidiummarket.common.eventstore.DomainEvent;
 import com.github.santosleijon.voidiummarket.purchaseorders.PurchaseOrder;
@@ -12,7 +13,10 @@ import java.util.UUID;
 public class PurchaseOrderDeleted extends DomainEvent {
 
     @JsonCreator
-    public PurchaseOrderDeleted(UUID id, Instant date, UUID aggregateId) {
+    public PurchaseOrderDeleted(
+            @JsonProperty("id") UUID id,
+            @JsonProperty("date") Instant date,
+            @JsonProperty("aggregateId") UUID aggregateId) {
         super(id, date, "PurchaseOrderDeleted", PurchaseOrder.aggregateName, aggregateId);
     }
 }
--- src/main/java/com/github/santosleijon/voidiummarket/saleorders/events/SaleOrderDeleted.java
+++ src/main/java/com/github/santosleijon/voidiummarket/saleorders/events/SaleOrderDeleted.java
@@ -1,6 +1,7 @@
 package com.github.santosleijon.voidiummarket.saleorders.events;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.github.santosleijon.voidiummarket.common.eventstore.DomainEvent;
 import com.github.santosleijon.voidiummarket.saleorders.SaleOrder;
@@ -12,7 +13,10 @@ import java.util.UUID;
 public class SaleOrderDeleted extends DomainEvent {
 
     @JsonCreator
-    public SaleOrderDeleted(UUID id, Instant date, UUID aggregateId) {
+    public SaleOrderDeleted(
+            @JsonProperty("id") UUID id,
+            @JsonProperty("date") Instant date,
+            @JsonProperty("aggregateId") UUID aggregateId) {
         super(id, date, "SaleOrderDeleted", SaleOrder.aggregateName, aggregateId);
     }
 }
